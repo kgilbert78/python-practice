a08e19fd5d01cb322e4d689a5aabc25b854b57c6
Kyle Gilbert <kgilbert78@gmail.com>
2021-12-11 17:21:34 -0500

    first files commited from my pc as a 2nd computer!

---
+++ python-virtual-environments/zelle_textbook/ch10-pc/roller.py
@@ -0,0 +1,19 @@
+from graphics import GraphWin, Point
+from button import Button
+
+def main():
+    win = GraphWin("Dice Roller")
+    win.setCoords(0, 0, 10, 10)
+    win.setBackground("green2")
+
+    rollButton = Button(win, Point(5, 4.5), 6, 1, "Roll Dice")
+    rollButton.activate()
+
+    quitButton = Button(win, Point(5, 1,), 2, 1, "Quit")
+
+    # pause to view window ( or to view until clicked anywhere, replace with win.getMouse() )
+    input("Press <Enter> to quit.")
+
+    win.close()
+
+main()
