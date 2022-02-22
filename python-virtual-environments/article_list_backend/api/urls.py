6a2ea573912b54559debea449796835f127387be
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2022-02-22 14:19:30 -0500

    convert to class based views

--- python-virtual-environments/article_list_backend/api/urls.py
+++ python-virtual-environments/article_list_backend/api/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
 # from .views import Index      # @step-12 comment this out
-from .views import index, article_list, article_details
+# from .views import index, article_list, article_details # @step-17 commented out
+from .views import ArticleList, Index, ArticleDetails
 
-#step-12 - after this visit http://localhost:8000/articles/ in the browser to see the data, and test get request with postman or insomnia. post will receive an error - see step 13 in views
+#step-17 - for class based views
 urlpatterns = [
-    path('', index),
-    path('articles/', article_list), # 12
-    path('articles/<int:primary_key>/', article_details), #step-15 (and import above)
+    path('', Index.as_view()),
+    path('articles/', ArticleList.as_view()),
+    path('articles/<int:id>/', ArticleDetails.as_view()),
 ]
+
+
+# # For function based views, commented out @step-17
+# #step-12 - after this visit http://localhost:8000/articles/ in the browser to see the data, and test get request with postman or insomnia. post will receive an error - see step 13 in views
+# urlpatterns = [
+#     path('', index),
+#     path('articles/', article_list), # 12
+#     path('articles/<int:primary_key>/', article_details), #step-15 (and import above)
+# ]
 
 # @step-12 comment this out
 # #step-2a (creation of this file and url patterns below)
--- python-virtual-environments/article_list_backend/api/views.py
+++ python-virtual-environments/article_list_backend/api/views.py
@@ -4,55 +4,107 @@ from django.http import JsonResponse # w/11
 from .models import Article # w/11
 from .serializers import ArticleSerializer # w/11
 from rest_framework.parsers import JSONParser # w/11
-from rest_framework.decorators import api_view # w/16
+# from rest_framework.decorators import api_view # w/16
 from rest_framework.response import Response # w/16
 from rest_framework import status # w/16
+from rest_framework.decorators import APIView # w/17
+
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('<pre>{"Articles backend status": "ok"}</pre>')
+# Class based views, #step-17
+class Index(APIView):
+    def get(self, request):
+        return Response({"Articles backend status": "ok"})
 
-
-@api_view(['GET', 'POST']) #step-16 (and import) - creates browsable api
-def article_list(request): #step-11 (and imports above)
-    if request.method == 'GET':
-        # get all articles - all() returns a copy of the query set, objects means class instances
+class ArticleList(APIView): # parent class APIView creates browsable api
+    def get(self, request):
         articles = Article.objects.all()
-        # pass serializer the data to turn into python native data types. to serialize a query set, add many=True (https://www.django-rest-framework.org/api-guide/serializers/#dealing-with-multiple-objects)
-        serializer = ArticleSerializer(articles, many=True)
+        serializer = ArticleSerializer(articles, many=True) # multiple articles
         return Response(serializer.data)
-
-    elif request.method =='POST':
-        # pass serializer the data to turn into python native data types. data=request.data https://treyhunner.com/2018/04/keyword-arguments-in-python/
+    
+    def post(self, request):
         serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) # from "status" imported above - displays description with number
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-#step-14, to see each article at http://localhost:8000/articles/<id>/
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def article_details(request, primary_key):
-    try:    # pk is key word, primary_key is argumument & path parameter in urls.py
-        article = Article.objects.get(pk=primary_key)
-    except Article.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
-    elif request.method =='PUT':
-        serializer = ArticleSerializer(article, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+class ArticleDetails(APIView):
+    def get_object(self, id): # function to find the instance with the id passed to it by route parameter in urls.py
+        try:
+            return Article.objects.get(id=id) # need key word arguments here
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def get(self, request, id):
+        article = self.get_object(id) # find instance
+        # serialize to turn (non-JSON, route param) queryset into python native data types: "serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that can then be easily rendered into JSON" https://www.django-rest-framework.org/api-guide/serializers/
+        serializer = ArticleSerializer(article)  # 1 article only here, no many=True
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id) # find instance
+        # pass the new data to article found, for update (overwrite)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id) # find instance
         article.delete() # change later to move to a deleted articles table
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT) 
+
+
+# Function based views:
+
+# def index(request):
+#     return HttpResponse('<pre>{"Articles backend status": "ok"}</pre>')
+
+
+# @api_view(['GET', 'POST']) #step-16 (and import) - creates browsable api
+# def article_list(request): #step-11 (and imports above)
+#     if request.method == 'GET':
+#         # get all articles - all() returns a copy of the query set, objects means class instances
+#         articles = Article.objects.all()
+#         # pass serializer the data to turn into python native data types. to serialize a query set, add many=True (https://www.django-rest-framework.org/api-guide/serializers/#dealing-with-multiple-objects)
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method =='POST':
+#         # pass serializer the data to turn into python native data types. data=request.data https://treyhunner.com/2018/04/keyword-arguments-in-python/
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED) # from "status" imported above - displays description with number
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# #step-14, to see each article at http://localhost:8000/articles/<id>/
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def article_details(request, primary_key):
+#     try:    # pk is key word, primary_key is argumument & path parameter in urls.py
+#         article = Article.objects.get(pk=primary_key)
+#     except Article.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+#     elif request.method =='PUT':
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'DELETE':
+#         article.delete() # change later to move to a deleted articles table
+#         return Response(status=status.HTTP_204_NO_CONTENT)
